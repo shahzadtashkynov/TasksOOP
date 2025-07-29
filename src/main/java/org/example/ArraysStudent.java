8059fea4ad381d4cca431f1909f9ca4460bd2a50
Shahzad <shahzadtashkynov@gmail.com>
2025-07-30 00:21:19 +0500

    Added Student class

---
+++ src/main/java/org/example/ArraysStudent.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ArraysStudent {
+    private String name;
+    private Integer [] ratings = { }; // in english ?
+
+    public ArraysStudent(String name, Integer [] ratings){
+        this.name = name;
+        this.ratings = ratings;
+    }
+    public String getName(){
+        return this.name;
+    }
+    @Override
+    public String toString() {
+        return "ArraysStudent{" +
+                "name='" + name + '\'' +
+                ", ratings=" + Arrays.toString(ratings) +
+                '}';
+    }
+}
