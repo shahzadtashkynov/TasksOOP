ca431f36a28021e9f72cc9fe8ce1c21379fb6279
Shahzad <shahzadtashkynov@gmail.com>
2025-07-30 00:43:56 +0500

    Added Point class

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Point {
+    private Integer x;
+    private Integer y;
+
+    public Point (int x, int y){
+        this.x = x ;
+        this.y = y ;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+}
